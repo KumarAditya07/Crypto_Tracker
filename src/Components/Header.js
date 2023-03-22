493d6450ed1059e7067a8398d76d75594db7ac8a
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2023-03-22 17:03:55 +0530

    Created contectApi to avid prop drilling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,7 @@ import {
   makeStyles,
   ThemeProvider,
 } from "@material-ui/core/styles";
+import { CryptoState } from "../CryptoContext";
 
 import React from "react";
 import { useNavigate } from 'react-router-dom';
@@ -36,6 +37,8 @@ const darkTheme = createTheme({
 const Header = () => {
   const classes = useStyles();
   const navigate = useNavigate();
+  const { currency, setCurrency } = CryptoState();
+
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -56,6 +59,8 @@ const Header = () => {
               height:40,
               marginRight:15,
             }}
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
             >
             <MenuItem value={"USD"}>USD</MenuItem>
             <MenuItem value={"INR"}>INR</MenuItem>
