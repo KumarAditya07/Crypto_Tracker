623078b53a85a07cfce0e9666986d2ceeb4be5da
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2023-08-05 10:21:07 +0530

    Carousel created and banner addded

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Banner from '../Components/Banner/Banner'
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>homepage</h1>
-    </div>
+    <>
+      <Banner />
+     
+    </>
   )
 }
 
